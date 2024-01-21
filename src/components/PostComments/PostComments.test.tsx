3e97927e46983bbfa45b1797388ed8f8c3f4e118
ruijosecj <ruijosecj@gmail.com>
2024-01-21 15:49:23 -0300

    Teste implementado

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -6,5 +6,15 @@ describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
-    });
+    })
+    test('Deve adicionar comentário na lista', () => {
+        render(<PostComment />)
+        fireEvent.change(screen.getByTestId('campo-comentario'), {
+            target: {
+                value: 'fez parte da minha infancia'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-comentar'))
+        expect(screen.getByText('fez parte da minha infancia')).toBeInTheDocument()    
+    })
 });
